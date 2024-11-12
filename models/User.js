ba4fe488935a4d3d37130429740a10aaa5be8de6
hajimonsur <taiwomonsuru29@gmail.com>
2024-11-12 17:25:05 +0100

    add user model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { type } = require("express/lib/response");
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    favorite: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Car" }],
+});
+
+module.exports = mongoose.model("User", UserSchema);
